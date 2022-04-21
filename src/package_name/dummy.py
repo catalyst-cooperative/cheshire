bc5790e5b3f42d18d0196340aeb58ec472085bbc
Zane Selvans <zane.selvans@catalyst.coop>
2022-04-20 23:42:14 -0500

    Add explicit type hints.

--- src/package_name/dummy.py
+++ src/package_name/dummy.py
@@ -4,7 +4,7 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-def do_something(a: int, b: int):
+def do_something(a: int = 0, b: int = 0) -> int:
     """A dummy function that does something."""
     logger.info("Don't just do something, stand there!")
     c = a + b
